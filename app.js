c8d8ce10f1d5591c892e32f693cc471ec198377f
parfait.ianis <parfait.ianis@gmail.com>
2021-11-17 19:28:01 +0100

    upload and res json file

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const DATAS = {
-    datas: [
-        {exercice: 'truc1', reps: '12', series:'54', repos: '1\'30'},
-        {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-        {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-        {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-        {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-        {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-        {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'}
-    ]
-}
+// const DATAS = {
+//     datas: [
+//         {exercice: 'truc1', reps: '12', series:'54', repos: '1\'30'},
+//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
+//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
+//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
+//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
+//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
+//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'}
+//     ]
+// }
 
-function init() {
+/* init writing table HTML */
+function init(datas) {
     const TABLE = document.querySelector('.table'),
         HEADERTABLE = document.querySelector('.header'),
         BODYTABLE = document.querySelector('.body')
 
-
-    writingHeader(HEADERTABLE)
-    writingBody(BODYTABLE)
+    writingHeader(datas, HEADERTABLE)
+    writingBody(datas, BODYTABLE)
 }
-
-function writingHeader(header) {
-    for (let x in DATAS) {
-        let value = DATAS[x],
+/* Writing in thead HTML */
+function writingHeader(datas, header) {
+    for (let x in datas) {
+        let value = datas[x],
             headers = Object.keys(value[0])
         for (let index = 0; index < headers.length; index++) {
             let th = document.createElement('th')
@@ -31,10 +31,10 @@ function writingHeader(header) {
         }
     }
 }
-
-function writingBody(body) {
-    for (let x in DATAS) {
-        let tab = DATAS[x]
+/* Writing in tbody HTML */
+function writingBody(datas, body) {
+    for (let x in datas) {
+        let tab = datas[x]
         for (let index = 0; index < tab.length; index++) {
             let tr = document.createElement('tr')
             let obj = tab[index]
@@ -46,10 +46,42 @@ function writingBody(body) {
             body.appendChild(tr)
         }
     }
+}
 
+/* read json file and output parsed datas to init() function */
+function changeHandler(evt) {
+    evt.stopPropagation()
+    evt.preventDefault()
+
+    var files = evt.target.files,
+        file = files[0],
+        fileReader = new FileReader()
+
+    fileReader.onload = function(progressEvent) {
+        var stringData = fileReader.result;
+        init(JSON.parse(stringData))
+    }
+
+    fileReader.onerror = function(progressEvent) {
+        appendLog("onerror!")
+        appendLog("Has Error!")
+    }
+
+    fileReader.readAsText(file, "UTF-8")
+}
+/* Output error */
+function appendLog(msg) {
+    document.getElementById('log-div').innerHTML += "<br>" + msg;
+}
+/* Check input change */
+function checkChange() {
+    const INPUT = document.querySelector('.file_upload')
+    INPUT.addEventListener('change', ev => {
+        changeHandler(ev)
+    })
 }
 
 
 (() => {
-    init()
+    checkChange()
 })();
