e9bd787c7bff6def4e2963e5f7cd68808b514b30
ianisparfait <parfait.ianis@gmail.com>
2024-05-25 15:56:30 +0200

    lil clean

--- app.js
+++ app.js
@@ -1,85 +1,98 @@
-// const DATAS = {
-//     datas: [
-//         {exercice: 'truc1', reps: '12', series:'54', repos: '1\'30'},
-//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'},
-//         {exercice: 'truc2', reps: '10', series:'54', repos: '1\'30'}
-//     ]
-// }
-
 /* init writing table HTML */
-function init(datas) {
-    const TABLE = document.querySelector('.table'),
-        HEADERTABLE = document.querySelector('.header'),
-        BODYTABLE = document.querySelector('.body')
+const init = (datas) => {
+    const TABLE = document.querySelector(".table"),
+        HEADERTABLE = document.querySelector(".header"),
+        BODYTABLE = document.querySelector(".body")
 
-    writingHeader(datas, HEADERTABLE)
-    writingBody(datas, BODYTABLE)
-}
+    writingHeader(datas, HEADERTABLE);
+    writingBody(datas, BODYTABLE);
+    handleSelection();
+};
+
 /* Writing in thead HTML */
-function writingHeader(datas, header) {
+const writingHeader = (datas, header) => {
     for (let x in datas) {
-        let value = datas[x],
-            headers = Object.keys(value[0])
+        const value = datas[x],
+            headers = Object.keys(value[0]);
         for (let index = 0; index < headers.length; index++) {
-            let th = document.createElement('th')
+            const th = document.createElement("th")
             th.innerHTML = headers[index]
             header.appendChild(th)
         }
+        const check = document.createElement("th");
+        header.prepend(check);
     }
-}
+};
+
 /* Writing in tbody HTML */
-function writingBody(datas, body) {
+const writingBody = (datas, body) => {
     for (let x in datas) {
-        let tab = datas[x]
+        const tab = datas[x];
         for (let index = 0; index < tab.length; index++) {
-            let tr = document.createElement('tr')
-            let obj = tab[index]
+            const tr = document.createElement("tr");
+            const obj = tab[index];
+            const check = document.createElement("td");
+            check.innerHTML = `<input type="checkbox" name="check" class="check" value="${index}">`;
+            tr.prepend(check);
             for (let i in obj) {
-                let td = document.createElement('td')
-                td.innerHTML = obj[i]
-                tr.appendChild(td)
+                const td = document.createElement("td")
+                td.innerHTML = obj[i];
+                tr.appendChild(td);
             }
-            body.appendChild(tr)
+            body.appendChild(tr);
         }
     }
 }
 
+/* Handle selection */
+const handleSelection = () => {
+    const checkbox = document.querySelectorAll(".check");
+    checkbox.forEach((element) => {
+        element.addEventListener("change", (e) => {
+            const tr = e.target.parentElement.parentElement;
+            if (e.target.checked) {
+                tr.classList.add("selected");
+            } else {
+                tr.classList.remove("selected");
+            }
+        });
+    });
+};
+
 /* read json file and output parsed datas to init() function */
-function changeHandler(evt) {
-    evt.stopPropagation()
-    evt.preventDefault()
+const changeHandler = (evt) => {
+    evt.stopPropagation();
+    evt.preventDefault();
 
     var files = evt.target.files,
         file = files[0],
-        fileReader = new FileReader()
+        fileReader = new FileReader();
 
     fileReader.onload = function(progressEvent) {
         var stringData = fileReader.result;
-        init(JSON.parse(stringData))
+        init(JSON.parse(stringData));
     }
 
     fileReader.onerror = function(progressEvent) {
-        appendLog("onerror!")
-        appendLog("Has Error!")
+        appendLog("onerror!");
+        appendLog("Has Error!");
     }
 
-    fileReader.readAsText(file, "UTF-8")
-}
+    fileReader.readAsText(file, "UTF-8");
+};
+
 /* Output error */
-function appendLog(msg) {
-    document.getElementById('log-div').innerHTML += "<br>" + msg;
-}
+const appendLog = (msg) => {
+    document.getElementById("log-div").innerHTML += "<br>" + msg;
+};
+
 /* Check input change */
-function checkChange() {
-    const INPUT = document.querySelector('.file_upload')
-    INPUT.addEventListener('change', ev => {
-        changeHandler(ev)
-    })
-}
+const checkChange = () => {
+    const INPUT = document.querySelector(".file_upload")
+    INPUT.addEventListener("change", ev => {
+        changeHandler(ev);
+    });
+};
 
 
 (() => {
